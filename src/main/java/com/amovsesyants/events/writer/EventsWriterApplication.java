ad4ed113c1308316072d00862ede4c20e14e810f
Artem Movsesyants <artmih1998@gmail.com>
2025-07-14 22:48:19 +0300

    Initial commit

---
+++ src/main/java/com/amovsesyants/events/writer/EventsWriterApplication.java
@@ -0,0 +1,13 @@
+package com.amovsesyants.events.writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsWriterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amovsesyants/events/writer/EventsWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amovsesyants.events.writer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsWriterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
